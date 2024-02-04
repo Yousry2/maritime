d81b7a40dfff1415ccb8836c6b70f4f2c48f769c
Mohamed Yousry <mohammed.yousry10@gmail.com>
2024-02-04 16:27:23 +0100

    add util-environment project , publish the app to github

--- apps/sea-routes/src/main.ts
+++ apps/sea-routes/src/main.ts
@@ -1,7 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { enableProdMode } from '@angular/core';
+import { environment } from '@maritime/util-environments';
 
-bootstrapApplication(AppComponent, appConfig).catch((err) =>
-  console.error(err)
-);
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
+
+if (environment.production) {
+     enableProdMode();
+}
